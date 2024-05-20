a313f15925c91d79dc371abe0ff29315f762e355
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-20 17:33:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 import app from './app.js';
 import cloudinary from 'cloudinary';
 
-const express = require('express');
-
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors (
-    {
-        origin : ["https://doc-booker-front-end.vercel.app/"],
-        methods : ["POST","GET","PUT","DELETE"],
-        credentials : true
-        
-    } 
-));
-
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
