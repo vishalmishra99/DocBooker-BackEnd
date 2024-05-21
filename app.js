625b52d9382f9cfafe0e7a14847e6c1a7e7bf86f
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-21 17:39:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-    origin: ["https://doc-booker-front-end.vercel.app/", "https://doc-booker-dashboard.vercel.app/login"],
+    origin: ["https://doc-booker-front-end.vercel.app", "https://doc-booker-dashboard.vercel.app/login"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
  })
